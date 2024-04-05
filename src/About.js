fd2c636d00d80243245e12e2b8255342747320eb
Yash Turambekar <yashdt12@icloud.com>
2024-04-05 23:28:38 +0530

    Changed meta information in index.html and added title and favicon

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <main className='About'>
       <p>
-        Feeling bored huh? Try something <Link to='/' className='highlight'>here</Link> to help you cheer up.
+        Feeling bored? Try something <Link to='/' className='highlight'>here</Link> to help you cheer up.
       </p>
     </main>
   )
