5ab81606ccc1ac406284c11f4aec597eb9b6dc9f
Yash Turambekar <yashdt12@icloud.com>
2024-04-07 12:17:36 +0530

    Added github link to about page

--- src/About.js
+++ src/About.js
@@ -6,6 +6,9 @@ const About = () => {
       <p>
         Feeling bored? Try something <Link to='/' className='highlight'>here</Link> to help you cheer up.
       </p>
+      <p>
+        Also checkout the <Link target='_blank' to='https://github.com/Yashdt12/i-am-bored' className='highlight'>github page</Link>.
+      </p>
     </main>
   )
 }
